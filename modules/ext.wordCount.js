5873df0cae2f3fc3d583bc0cd1180d981c7a616c
John Leen <jleen@saturnvalley.org>
2020-06-09 22:46:31 -0400

    Strip wiki markup before counting words

--- modules/ext.wordCount.js
+++ modules/ext.wordCount.js
@@ -2,17 +2,31 @@
     $(function () {
 		document.getElementById('wpTextbox1').onkeyup = function () {
 			var article = document.getElementById('wpTextbox1').value;
-			var words = 0;
-			var wasLetter = false;
-			for (var i = 0; i < article.length; ++i) {
-				var c = article[i];
-				var isLetter = ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <='z'));
-				if (isLetter && !wasLetter)
-					++words;
-				wasLetter = isLetter;
-			}
+            var words = wordCount(stripMarkup(article));
 			document.getElementById('mw-js-message').style.display = 'initial';
 			document.getElementById('mw-js-message').innerHTML = 'Word count: ' + words;
 		};
     });
 }(jQuery, mediaWiki));
+
+function stripMarkup(str) {
+    str = str.replace(/\[\[[^|\]]*\|/gi, '');
+    str = str.replace(/\[\[/gi, '');
+    str = str.replace(/\]\]/gi, '');
+    str = str.replace(/'''/gi, '');
+    str = str.replace(/''/gi, '');
+    return str;
+}
+
+function wordCount(str) {
+    var words = 0;
+    var wasLetter = false;
+    for (var i = 0; i < str.length; ++i) {
+        var c = str[i];
+        var isLetter = ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <='z'));
+        if (isLetter && !wasLetter)
+            ++words;
+        wasLetter = isLetter;
+    }
+    return words;
+}
